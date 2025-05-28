794049877626fc833f940307275193371ed53d28
Arie Hofland <arie_hofland@hotmail.com>
2025-05-28 19:28:17 +0200

    Erroring Microsoft Analyzers

---
+++ MicrosoftAnalyzersBug/MicrosoftAnalyzersBug/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace ArmaBotCs;
+
+internal sealed class Program
+{
+    private Program() { }
+
+    public static void Main(string[] args)
+    {
+        // not relevant
+    }
+}
